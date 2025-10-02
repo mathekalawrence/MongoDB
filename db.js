62d899d01ad001a3355c62a47bfbd589aafcfe75
mathekalawrence <mathekalawrence96@gmail.com>
2025-10-02 15:46:55 +0300

    Getting towards it

--- db.js
+++ db.js
@@ -2,3 +2,8 @@ const mongoose = require('mongoose') //common JS module/ the Default
 
 //import mongoose from 'mongoose' //ECMAScript module
 require('dotenv').config();
+
+async function connectDB() {
+    await mongoose.connect(process.env.MONGODBATLAS_URI)
+    
+}
